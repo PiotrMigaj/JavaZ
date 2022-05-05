fdfc2e478e9147318f07d8e2feae0364cc36ab70
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-05-05 16:44:45 +0200

    New commit

---
+++ src/main/java/pl/uczen/lambdy/Initiable.java
@@ -0,0 +1,6 @@
+package pl.uczen.lambdy;
+
+@FunctionalInterface
+public interface Initiable {
+    void init(Integer i);
+}
--- src/main/java/pl/uczen/lambdy/Main.java
+++ src/main/java/pl/uczen/lambdy/Main.java
@@ -11,13 +11,24 @@ public class Main {
         });
 
         String amazingHello = "Hello Peter!";
-
         hello("Cześć Piotr", ()->System.out.println(amazingHello));
+
+
+        init("Super lambdy",i->{
+            for (int j=0;j<i;j++){
+                System.out.println(j);
+            }
+        });
     }
 
     public static void hello(String s, Helloable helloable) {
         System.out.println(s);
         helloable.sayHello();
+    }
+
+    public static void init(String s, Initiable initiable){
+        System.out.println(s);
+        initiable.init(45);
     }
 
 }
